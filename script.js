571a5ef336fa4a90688804db25011a6b194d5edb
sehyun <sehyun.lee217@gmail.com>
2023-04-20 00:07:15 +0900

    Cleaned up script.js, added color-changing functionality, and redid the styles.css

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//  ----- Declare Empty Library --------- 
 let myLibrary = [];
 
 // ----- Declare Book Object ------- 
@@ -7,10 +8,16 @@ function Book(title, author, num_pages, read) {
     this.author = author;
     this.num_pages = num_pages;
     this.read = read;
-}
+};
 
-Book.prototype.printData = function () {
-    console.log(this.title, this.author, this.num_pages, this.read);
+// ----- function that changes status of Book ------- 
+Book.prototype.changeReadstatus = function () {
+    if (this.read == "Read") {
+        this.read = "Not Read";
+    }
+    else {
+        this.read = "Read";
+    }
 };
 
 function addBookToLibrary(book, library) {
@@ -19,28 +26,28 @@ function addBookToLibrary(book, library) {
 }
 
 function displayLibrary(library) {
+    clearDisplay();
     // select the table in HTML
     const tableBody = document.querySelector("tbody");
-
     // access data from myLibrary and convert it to td
     for (let book of library) {
-
         // ------ Add Data to Table -----------
-
-        // add new row to table & link to data to the row
+        // create new row with class="newRow"
         const tableRow = document.createElement("tr");
-        tableBody.appendChild(tableRow);
 
         tableRow.classList.add("newRow");
+        // add new row to table
+        tableBody.appendChild(tableRow);
 
+        // add title as a table data to new row
         const titleData = document.createElement("td");
         titleData.textContent = book.title;
         tableRow.appendChild(titleData);
-
+        // add author as a table data to new row
         const authorData = document.createElement("td");
         authorData.textContent = book.author;
         tableRow.appendChild(authorData);
-
+        // add page numbers as a table data to new row
         const pageData = document.createElement("td");
         pageData.textContent = book.num_pages;
         tableRow.appendChild(pageData);
@@ -49,29 +56,29 @@ function displayLibrary(library) {
         const statusTd = document.createElement("td");
         statusTd.classList.add("statusBtn");
         const statusBtn = document.createElement("button");
-
-        statusTd.appendChild(statusBtn);
         statusBtn.textContent = book.read;
+
+        // change color of book depending on status of book being read
+        if (book.read == "Not Read") {
+            statusBtn.setAttribute("class", "red");
+        }
+        else {  // when book.read == "Read"
+            statusBtn.setAttribute("class", "green");
+        }
+
+        // Add status button element to the new row
+        statusTd.appendChild(statusBtn);
         tableRow.appendChild(statusTd);
 
+        // using data attribute to access myLibrary
         statusTd.setAttribute("data-title", book.title);
-
-
         // ------ Status Functionality ----------
 
         // on click event of status button,
-        statusTd.addEventListener("click", (e) => {
-            for (let book of myLibrary) {
-                if (book["title"] == statusTd.dataset["title"]) {
-                    if (book.read == "Read") {
-                        book.read = "Not Read";
-                    }
-                    else {
-                        book.read = "Read";
-                    }
-                }
-            }
-            clearDisplay();
+        statusTd.addEventListener("click", function () {
+            // change status of book being read
+            book.changeReadstatus();
+            // display new table with new status of book
             displayLibrary(myLibrary);
         });
 
@@ -103,6 +110,8 @@ function displayLibrary(library) {
     }
 }
 
+// ----- remove all table rows from display ------- 
+
 function clearDisplay() {
     const tableBody = document.querySelector("tbody");
     const allNewrows = document.querySelectorAll(".newRow");
@@ -111,7 +120,24 @@ function clearDisplay() {
     }
 }
 
-// ----- When Form is submitted(Main Action) ----- 
+// ----- When + button is clicked ------ 
+
+const addBookbtn = document.getElementById("add_book");
+
+// show the form 
+addBookbtn.addEventListener("click", () => {
+    const form = document.getElementById("submitForm");
+
+    if (form.style.visibility == "hidden") {
+        form.style.visibility = "visible";
+    }
+    else {
+        form.style.visibility = "hidden";
+    }
+
+});
+
+// ----- form is submitted(Main Action) ----- 
 
 const submitForm = document.querySelector("form");
 
@@ -128,18 +154,13 @@ submitForm.addEventListener("submit", event => {
 
     addBookToLibrary(newBook, myLibrary);
 
+    // clear previous items after pressing submit btn
     event.target["submitTitle"].value = "";
     event.target["submitAuthor"].value = "";
     event.target["submitPages"].value = "";
 
-    clearDisplay();
     displayLibrary(myLibrary);
 });
-
-// const newBook2 = new Book("Title", "Author", 343, "Not Read");
-// addBookToLibrary(newBook2, myLibrary);
-// displayLibrary(myLibrary);
-
 
 // ------ Additional Form validation using Constraint Validation API ----
 
